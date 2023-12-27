b3ea27216061091e821fb6528cc496d7d2d90b8b
Safouane El Ghazouali <54261127+safouane95@users.noreply.github.com>
2023-12-27 14:05:57 +0100

    Readme

---
+++ README.mk
@@ -0,0 +1,33 @@
+# TomCat et JerryMouse
+
+Ce jeu🎮 simple en 🐍Python🐍 utilise la bibliothèque Pygame pour créer une interface graphique où l'objectif est de déplacer le chat vers la souris dans une grille de 12x12. Voici comment utiliser et exécuter le jeu.
+
+### Installation
+Requis: Python3
+
+Assurez-vous d'avoir Python et pip installés sur votre système. Vous pouvez installer les dépendances nécessaires en utilisant la commande suivante dans votre terminal :
+
+```pip install pygame```
+
+### Exécution du jeu
+Exécutez le script game.py en utilisant la commande suivante dans votre terminal :
+
+```python game.py```
+ou
+```python3 game.py```
+
+### Instructions du jeu
+Utilisez les touches fléchées pour déplacer le chat vers le haut, le bas, la gauche ou la droite.
+Évitez les bords de la grille et essayez d'attraper la souris qui apparaît aléatoirement.
+
+### Structure du code
+utils.py: Contient des fonctions utilitaires pour la gestion du tableau et le déplacement du chat.
+game.py: Le script principal qui utilise Pygame pour créer l'interface graphique du jeu.
+
+### Commentaires dans le code
+Les commentaires ont été ajoutés pour expliquer chaque fonction et partie du code.
+Des messages informatifs sont affichés dans la console pour indiquer si le chat a attrapé la souris.
+### Personnalisation du jeu
+Vous pouvez personnaliser la taille de la fenêtre, la taille des cases, les couleurs et les images du chat et de la souris en modifiant les paramètres dans le script game.py.
+
+Amusez-vous bien en jouant à Chat et Souris!
